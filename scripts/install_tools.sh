6f1324d60f2ea557b6150303f01723158c2e1cfc
Erik Skopp <info@erik-skopp.de>
2026-03-16 23:50:04 +0100

    Add Bluetooth launcher for Waybar

--- scripts/install_tools.sh
+++ scripts/install_tools.sh
@@ -12,17 +12,20 @@ main() {
     swappy \
     brightnessctl \
     pavucontrol \
-    wtype
+    wtype \
+    blueman
 
   link_file "${REPO_DIR}/bin/screenshot-region" "${HOME}/.local/bin/screenshot-region"
   link_file "${REPO_DIR}/bin/power-menu" "${HOME}/.local/bin/power-menu"
   link_file "${REPO_DIR}/bin/clipboard-menu" "${HOME}/.local/bin/clipboard-menu"
   link_file "${REPO_DIR}/bin/clipboard-daemon" "${HOME}/.local/bin/clipboard-daemon"
+  link_file "${REPO_DIR}/bin/bluetooth-menu" "${HOME}/.local/bin/bluetooth-menu"
 
   make_executable "${REPO_DIR}/bin/screenshot-region"
   make_executable "${REPO_DIR}/bin/power-menu"
   make_executable "${REPO_DIR}/bin/clipboard-menu"
   make_executable "${REPO_DIR}/bin/clipboard-daemon"
+  make_executable "${REPO_DIR}/bin/bluetooth-menu"
 }
 
 main "$@"
