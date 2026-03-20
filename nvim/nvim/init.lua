e81d45320b9e4762818035c83160287d09aa881a
Erik Skopp <info@erik-skopp.de>
2026-03-20 13:23:37 +0100

    Expand minimal Neovim init config

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,1 +1,7 @@
 vim.opt.number = true
+vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+vim.cmd("highlight NormalNC guibg=NONE ctermbg=NONE")
+vim.cmd("highlight EndOfBuffer guibg=NONE ctermbg=NONE")
+
+vim.opt.relativenumber = false
+vim.opt.mouse = "a"
