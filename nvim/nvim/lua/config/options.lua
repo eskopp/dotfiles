e95b8e2aa8ba27de1c1c88b9450de892d291cff6
Erik Skopp <info@erik-skopp.de>
2026-03-20 13:48:57 +0100

    Add Telescope-based Neovim setup

---
+++ nvim/nvim/lua/config/options.lua
@@ -0,0 +1,31 @@
+-- Show absolute line numbers.
+vim.opt.number = true
+
+-- Do not use relative line numbers.
+vim.opt.relativenumber = false
+
+-- Enable mouse support in all modes.
+vim.opt.mouse = "a"
+
+-- Keep undo history between sessions.
+vim.opt.undofile = true
+
+-- Use the system clipboard.
+vim.opt.clipboard = "unnamedplus"
+
+-- Use case-insensitive search unless uppercase is used.
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep a few lines visible above and below the cursor.
+vim.opt.scrolloff = 5
+
+-- Use spaces instead of tabs.
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+-- Make the Neovim background transparent.
+vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+vim.cmd("highlight NormalNC guibg=NONE ctermbg=NONE")
+vim.cmd("highlight EndOfBuffer guibg=NONE ctermbg=NONE")
