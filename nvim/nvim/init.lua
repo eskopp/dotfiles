188c7e76b12df05d4701063896c31df96f308f19
Erik Skopp <info@erik-skopp.de>
2026-03-20 13:26:02 +0100

    Document Neovim init configuration

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,7 +1,13 @@
+-- Show normal line numbers.
 vim.opt.number = true
+
+-- Make the Neovim background transparent.
 vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
 vim.cmd("highlight NormalNC guibg=NONE ctermbg=NONE")
 vim.cmd("highlight EndOfBuffer guibg=NONE ctermbg=NONE")
 
+-- Do not use relative line numbers.
 vim.opt.relativenumber = false
+
+-- Enable mouse support.
 vim.opt.mouse = "a"
