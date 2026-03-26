9c8abb8317f233c8e4b4fc6fce4135cedc38e989
Erik Skopp <info@erik-skopp.de>
2026-03-26 21:04:45 +0100

    Refine Neovim config loading and Waybar setup

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -1,3 +1,13 @@
-require("config.options")
-require("config.keymaps")
-require("config.lazy")
+-- Show normal line numbers.
+vim.opt.number = true
+
+-- Make the Neovim background transparent.
+vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
+vim.cmd("highlight NormalNC guibg=NONE ctermbg=NONE")
+vim.cmd("highlight EndOfBuffer guibg=NONE ctermbg=NONE")
+
+-- Do not use relative line numbers.
+vim.opt.relativenumber = false
+
+-- Enable mouse support.
+vim.opt.mouse = "a"
