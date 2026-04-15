424e417e780e8fe3d1973b697253fcf95fb29f33
Erik Skopp <info@erik-skopp.de>
2026-04-15 20:05:18 +0200

    Nord Wallpaper

--- nvim/nvim/lua/plugins/colorschemes.lua
+++ nvim/nvim/lua/plugins/colorschemes.lua
@@ -4,19 +4,4 @@ return {
     lazy = false,
     priority = 1000,
   },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        transparent_background = true,
-        integrations = {
-          telescope = true,
-        },
-      })
-    end,
-  },
 }
